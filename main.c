d9f1be23a9e54ac779f5194889a732cfc778fc35
X123M3-256 <edward@knaphill.me.uk>
2015-02-20 14:57:23 +0000

    Added FPS counter

--- main.c
+++ main.c
@@ -20,7 +20,6 @@ grid_apply_gravity(grid,0.05);
 grid_project(grid,0.05);
 //Compute velocity delta
 grid_calculate_velocity_delta(grid);
-
 //Update particle velocities using new grid velocities
 grid_transfer_to_particle_system(grid,particles);
 //Advect particles
@@ -48,11 +47,11 @@ float u;
     r=100,g=100,b=100;
     break;
     case FLUID:
-    u=0.0;//GRID_CELL(grid,x,y).pressure/200000.0;
+    u=GRID_CELL(grid,x,y).pressure;///200000.0;
         if(u>1)u=1;
         else if(u<-1)u=-1;
         if(u>=0)r=0,g=255*u,b=255*(1.0-u);
-        else r=-u*255,g=0,b=255*(u-1.0);
+        else r=-u*255,g=0,b=255*(1.0+u);
     break;
     case EMPTY:
     r=0,g=0,b=0;
@@ -89,6 +88,8 @@ int i,x,y;
     for(y=0;y<grid->height;y++)
     for(x=0;x<grid->width;x++)
     {
+    //render_cell(screen,grid,x,y);
+        /*
         if(GRID_CELL(grid,x,y).type==SOLID)
         {
             for(i=0;i<4;i++)
@@ -99,12 +100,13 @@ int i,x,y;
             put_pixel(screen,x*4+3,y*4+i,80,80,80);
             }
         }
+    */
     }
     for(i=0;i<particles->num_particles;i++)
     {
     float u=0.1*sqrt(particles->particles[i].velocity_x*particles->particles[i].velocity_x+particles->particles[i].velocity_y*particles->particles[i].velocity_y);
         if(u>1)u=1;
-    put_pixel(screen,(int)((particles->particles[i].position_x+0.5)*4.0),(int)((particles->particles[i].position_y+0.5)*4.0),255*u,255*u,255);
+    put_pixel(screen,(int)((particles->particles[i].position_x+0.5)*8.0),(int)((particles->particles[i].position_y+0.5)*8.0),255*u,255*u,255);
     }
 SDL_UnlockSurface(screen);
 SDL_Flip(screen);
@@ -112,28 +114,35 @@ SDL_Flip(screen);
 
 int main(int argc,char* argv[])
 {
-
 SDL_Init(SDL_INIT_VIDEO);
 freopen( "CON", "w", stdout );
 atexit(SDL_Quit);
 
 
 
-grid_t* grid=grid_new(200,50);
+grid_t* grid=grid_new(300,150);
 
 particle_system_t* particles=particle_system_new(16192);
 
 //particle_system_populate_rectangle(particles,1,20,49,49);
 
-//grid_set_rectangle(grid,SOLID,0,35,30,49);
-//grid_set_rectangle(grid,SOLID,150,35,150,49);
-//particle_system_populate_rectangle(particles,30,40,149,49);
+/*
+grid_set_rectangle(grid,SOLID,0,35,30,49);
+grid_set_rectangle(grid,SOLID,150,40,150,49);
+particle_system_populate_rectangle(particles,30,40,149,49);
+*/
 
+/*
+grid_set_rectangle(grid,SOLID,20,0,20,45);
+grid_set_rectangle(grid,SOLID,150,41,150,49);
+particle_system_populate_rectangle(particles,1,10,19,49);
+*/
 //particle_system_populate_rectangle(particles,1,40,299,49);
 //particle_system_populate_rectangle(particles,0,10,30,40);
 
-//particle_system_populate_rectangle(particles,1,40,299,49);
-//particle_system_populate_rectangle(particles,145,5,155,15);
+particle_system_populate_rectangle(particles,1,140,299,149);
+particle_system_populate_rectangle(particles,145,5,155,15);
+
 /*
 grid_set_rectangle(grid,SOLID,0,15,30,49);
 grid_set_rectangle(grid,SOLID,30,25,60,49);
@@ -141,10 +150,8 @@ grid_set_rectangle(grid,SOLID,60,20,60,49);
 particle_system_populate_rectangle(particles,61,30,149,49);
 */
 
-//particle_system_populate_rectangle(particles,1,90,299,99);
-//particle_system_populate_rectangle(particles,145,5,155,15);
 
-
+/*
 grid_set_rectangle(grid,SOLID,100,45,199,49);
 grid_set_rectangle(grid,SOLID,90,47,199,49);
 grid_set_rectangle(grid,SOLID,110,43,199,49);
@@ -154,17 +161,20 @@ particle_system_populate_rectangle(particles,100,40,110,45);
 particle_system_populate_rectangle(particles,110,40,199,43);
 particle_system_populate_rectangle(particles,0,20,20,40);
 particle_system_populate_rectangle(particles,20,30,30,40);
+*/
 
+SDL_Surface* screen=SDL_SetVideoMode(grid->width*8,grid->height*8,32,SDL_DOUBLEBUF);
 
-SDL_Surface* screen=SDL_SetVideoMode(grid->width*4,grid->height*4,32,SDL_DOUBLEBUF);
-
-int timer=0;
+int milliseconds=SDL_GetTicks();
+int frames=0;
     while(!SDL_GetKeyState(NULL)[SDLK_SPACE])
     {
+    frames++;
     SDL_PumpEvents();
     compute_simulation_step(particles,grid);
-        //if(timer++%5==0)particle_system_populate_rectangle(particles,1,2,15,4);
+        //if(timer++%5==0)particle_system_populate_rectangle(particles,1,2,4,4);
     render(screen,grid,particles);
     }
+printf("FPS %f\n",1000.0*(float)frames/(float)(SDL_GetTicks()-milliseconds));
 return 0;
 }
