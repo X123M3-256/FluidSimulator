094a3a461c71fd68b57aa948d3d48da3bed4373b
X7123M3-256 <X123M3-256@users.noreply.github.com>
2015-02-21 23:29:09 +0000

    Moved rendering code into interface.c

---
+++ interface.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include "interface.h"
+
+interface_t* interface_new(simulation_t* simulation)
+{
+interface_t* interface=malloc(sizeof(interface_t));
+interface->simulation=simulation;
+interface->mouse_x=0.0;
+interface->mouse_y=0.0;
+interface->mouse_pressed=0;
+return interface;
+}
+
+
+void interface_process_events(interface_t* interface)
+{
+SDL_Event event;
+    while(SDL_PollEvent(&event)!=0)
+    {
+        if(event.type==SDL_MOUSEMOTION)
+        {
+        interface->mouse_x=(float)event.motion.x/4.0;
+        interface->mouse_y=(float)event.motion.y/4.0;
+        }
+        else if(event.type==SDL_MOUSEBUTTONDOWN)
+        {
+        interface->drag_x=interface->mouse_x;
+        interface->drag_y=interface->mouse_y;
+        }
+        else if(event.type==SDL_MOUSEBUTTONUP)
+        {
+        particle_system_populate_rectangle(interface->simulation->particle_system,interface->drag_x,interface->drag_y,interface->mouse_x,interface->mouse_y);
+        }
+    }
+}
+
+void put_pixel(SDL_Surface* screen,int x,int y,char r,char g,char b)
+{
+    if(x<0||x>=screen->w||y<0||y>=screen->h)return;
+char* pixel=screen->pixels+y*screen->pitch+4*x;
+pixel[0]=b;
+pixel[1]=g;
+pixel[2]=r;
+}
+
+void interface_render(interface_t* interface,SDL_Surface* screen)
+{
+SDL_Rect all;
+all.x=0;
+all.y=0;
+all.w=screen->w;
+all.h=screen->h;
+SDL_FillRect(screen,&all,0);
+SDL_LockSurface(screen);
+
+int i,x,y;
+    for(y=0;y<interface->simulation->grid->height;y++)
+    for(x=0;x<interface->simulation->grid->width;x++)
+    {
+        if(GRID_CELL(interface->simulation->grid,x,y).type==SOLID)
+        {
+
+            for(i=0;i<4;i++)
+            {
+            put_pixel(screen,x*4+i,y*4,80,80,80);
+            put_pixel(screen,x*4+i,y*4+3,80,80,80);
+            put_pixel(screen,x*4,y*4+i,80,80,80);
+            put_pixel(screen,x*4+3,y*4+i,80,80,80);
+            }
+        }
+    }
+    for(i=0;i<interface->simulation->particle_system->num_particles;i++)
+    {
+    particle_t* particle=interface->simulation->particle_system->particles+i;
+
+    float u=0.1*sqrt(particle->velocity_x*particle->velocity_x+particle->velocity_y*particle->velocity_y);
+        if(u>=1.0)u=1.0;
+    put_pixel(screen,(int)((particle->position_x+0.5)*4.0),(int)((particle->position_y+0.5)*4.0),(int)(255.0*u),(int)(255.0*u),255);
+    }
+SDL_UnlockSurface(screen);
+}
+
+
+/*
+void render_cell(SDL_Surface* screen,grid_t* grid,int x,int y)
+{
+int sx=x*8;
+int sy=y*8;
+char r,g,b;
+
+float u;
+    switch(GRID_CELL(grid,x,y).type)
+    {
+    case SOLID:
+    r=100,g=100,b=100;
+    break;
+    case FLUID:
+    u=GRID_CELL(grid,x,y).pressure;///200000.0;
+        if(u>1)u=1;
+        else if(u<-1)u=-1;
+        if(u>=0)r=0,g=255*u,b=255*(1.0-u);
+        else r=-u*255,g=0,b=255*(1.0+u);
+    break;
+    case EMPTY:
+    r=0,g=0,b=0;
+    break;
+    }
+int i;
+    for(i=0;i<8;i++)
+    {
+    put_pixel(screen,sx+i,sy,r,g,b);
+    put_pixel(screen,sx+i,sy+7,r,g,b);
+    put_pixel(screen,sx,sy+i,r,g,b);
+    put_pixel(screen,sx+7,sy+i,r,g,b);
+    }
+int x_vel_mag=(int)floor(0.1*GRID_VELOCITY_X(grid,MINUS_HALF(x),y));
+int y_vel_mag=(int)floor(0.1*GRID_VELOCITY_Y(grid,x,MINUS_HALF(y)));
+
+    if(x_vel_mag>=0)for(i=0;i<x_vel_mag;i++)put_pixel(screen,sx+i,sy+4,255,0,0);
+    else for(i=0;i>x_vel_mag;i--)put_pixel(screen,sx+i,sy+4,255,0,0);
+
+    if(y_vel_mag>=0) for(i=0;i<y_vel_mag;i++)put_pixel(screen,sx+4,sy+i,255,0,0);
+    else for(i=0;i>y_vel_mag;i--)put_pixel(screen,sx+4,sy+i,255,0,0);
+
+}
+*/
---
+++ interface.h
@@ -0,0 +1,18 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+#include "simulation.h"
+
+typedef struct
+{
+simulation_t* simulation;
+float mouse_x;
+float mouse_y;
+float drag_x;
+float drag_y;
+unsigned int mouse_pressed;
+}interface_t;
+
+interface_t* interface_new(simulation_t* simulation);
+void interface_process_events(interface_t* interface);
+void interface_render(interface_t* interface,SDL_Surface* screen);
+#endif // INTERFACE_H_INCLUDED
